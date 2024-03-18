990478b657926e73f01c07052ed13b4695a92225
mickle228 <olehkachan05@gmail.com>
2024-03-18 11:12:33 +0200

    initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {RouterProvider} from "react-router-dom";
+
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {router} from "./router";
+import {SearchProvider, ThemeProvider} from "./hoc";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ThemeProvider>
+        <SearchProvider>
+            <RouterProvider router={router}/>
+        </SearchProvider>
+    </ThemeProvider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
